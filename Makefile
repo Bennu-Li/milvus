f8686672e7282c1f8cb0c462e8dcc480c1a3febb
Bennu-Li <yunmei.li@zilliz.com>
2023-03-14 19:31:32 +0800

    Add groovy for gpu-ci
    Signed-off-by: Bennu-Li <yunmei.li@zilliz.com>

--- Makefile
+++ Makefile
@@ -40,7 +40,7 @@ milvus-gpu: build-cpp-gpu print-build-info
 	@source $(PWD)/scripts/setenv.sh && \
 		mkdir -p $(INSTALL_PATH) && go env -w CGO_ENABLED="1" && \
 		GO111MODULE=on $(GO) build -ldflags="-r $${RPATH} -X '$(OBJPREFIX).BuildTags=$(BUILD_TAGS)' -X '$(OBJPREFIX).BuildTime=$(BUILD_TIME)' -X '$(OBJPREFIX).GitCommit=$(GIT_COMMIT)' -X '$(OBJPREFIX).GoVersion=$(GO_VERSION)'" \
-		${AARCH64_FLAG} -o $(INSTALL_PATH)/milvus-gpu $(PWD)/cmd/main.go 1>/dev/null
+		${AARCH64_FLAG} -o $(INSTALL_PATH)/milvus $(PWD)/cmd/main.go 1>/dev/null
 
 get-build-deps:
 	@(env bash $(PWD)/scripts/install_deps.sh)
--- ci/jenkins/PRGPU.groovy
+++ ci/jenkins/PRGPU.groovy
@@ -73,6 +73,152 @@ pipeline {
                 }
             }
         }
+
+        stage('Install & E2E Test') {
+            matrix {
+                axes {
+                    axis {
+                        name 'MILVUS_SERVER_TYPE'
+                        values 'standalone', 'distributed'
+                    }
+                    axis {
+                        name 'MILVUS_CLIENT'
+                        values 'pymilvus'
+                    }
+                }
+
+                stages {
+                    stage('Install') {
+                        steps {
+                            container('main') {
+                                stash includes: 'tests/**', name: 'testCode', useDefaultExcludes: false
+                                dir ('tests/scripts') {
+                                    script {
+                                        sh 'printenv'
+                                        def clusterEnabled = "false"
+                                        if ("${MILVUS_SERVER_TYPE}" == 'distributed') {
+                                            clusterEnabled = "true"
+                                        }
+                                        sh 'git config --global --add safe.directory /home/jenkins/agent/workspace'
+
+                                        if ("${MILVUS_CLIENT}" == "pymilvus") {
+                                            if ("${imageTag}"==''){
+                                                dir ("imageTag"){
+                                                    try{
+                                                        unstash 'imageTag'
+                                                        imageTag=sh(returnStdout: true, script: 'cat imageTag.txt | tr -d \'\n\r\'')
+                                                    }catch(e){
+                                                        print "No Image Tag info remained ,please rerun build to build new image."
+                                                        exit 1
+                                                    }
+                                                }
+                                            }
+                                            withCredentials([usernamePassword(credentialsId: "${env.CI_DOCKER_CREDENTIAL_ID}", usernameVariable: 'CI_REGISTRY_USERNAME', passwordVariable: 'CI_REGISTRY_PASSWORD')]){
+                                                sh """
+                                                MILVUS_CLUSTER_ENABLED=${clusterEnabled} \
+                                                MILVUS_HELM_REPO="http://nexus-nexus-repository-manager.nexus:8081/repository/milvus-proxy" \
+                                                TAG=${imageTag}\
+                                                ./e2e-k8s.sh \
+                                                --skip-export-logs \
+                                                --skip-cleanup \
+                                                --skip-setup \
+                                                --skip-test \
+                                                --skip-build \
+                                                --skip-build-image \
+                                                --install-extra-arg "
+                                                --set etcd.metrics.enabled=true \
+                                                --set etcd.metrics.podMonitor.enabled=true \
+                                                --set indexCoordinator.gc.interval=1 \
+                                                --set indexNode.disk.enabled=true \
+                                                --set queryNode.disk.enabled=true \
+                                                --set standalone.disk.enabled=true \
+                                                --version ${chart_version} \
+                                                -f values/ci/pr-gpu.yaml" 
+                                                """
+                                            }
+                                        } else {
+                                            error "Error: Unsupported Milvus client: ${MILVUS_CLIENT}"
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    stage('E2E Test'){
+                        options { 
+                            skipDefaultCheckout() 
+                        }
+                        agent {
+                                kubernetes {
+                                    inheritFrom 'default'
+                                    defaultContainer 'main'
+                                    yamlFile 'ci/jenkins/pod/e2e.yaml'
+                                    customWorkspace '/home/jenkins/agent/workspace'
+                                }
+                        }
+                        steps {
+                            container('pytest') {
+                                unstash('testCode')
+                                script {
+                                        sh 'ls -lah'
+                                }
+                                dir ('tests/scripts') {
+                                    script {
+                                        def release_name=sh(returnStdout: true, script: './get_release_name.sh')
+                                        def clusterEnabled = 'false'
+                                        if ("${MILVUS_SERVER_TYPE}" == "distributed") {
+                                            clusterEnabled = "true"
+                                        }
+                                        if ("${MILVUS_CLIENT}" == "pymilvus") {
+                                            sh """
+                                            MILVUS_HELM_RELEASE_NAME="${release_name}" \
+                                            MILVUS_HELM_NAMESPACE="milvus-ci" \
+                                            MILVUS_CLUSTER_ENABLED="${clusterEnabled}" \
+                                            TEST_TIMEOUT="${e2e_timeout_seconds}" \
+                                            ./ci_e2e.sh  "-n 6 -x --tags L0 L1 --timeout ${case_timeout_seconds}"
+                                            """
+                            
+                                        } else {
+                                        error "Error: Unsupported Milvus client: ${MILVUS_CLIENT}"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        post{
+                            always {
+                                container('pytest'){
+                                    dir("${env.ARTIFACTS}") {
+                                            sh "tar -zcvf ${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz /tmp/ci_logs/test --remove-files || true"
+                                            archiveArtifacts artifacts: "${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${MILVUS_CLIENT}-pytest-logs.tar.gz ", allowEmptyArchive: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                post{
+                    always {
+                        container('main') {
+                            dir ('tests/scripts') {  
+                                script {
+                                    def release_name=sh(returnStdout: true, script: './get_release_name.sh')
+                                    sh "kubectl get pods -n ${MILVUS_HELM_NAMESPACE} | grep ${release_name} "
+                                    sh "./uninstall_milvus.sh --release-name ${release_name}"
+                                    sh "./ci_logs.sh --log-dir /ci-logs  --artifacts-name ${env.ARTIFACTS}/artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-${MILVUS_CLIENT}-e2e-logs \
+                                    --release-name ${release_name}"
+                                    dir("${env.ARTIFACTS}") {
+                                        archiveArtifacts artifacts: "artifacts-${PROJECT_NAME}-${MILVUS_SERVER_TYPE}-${SEMVER}-${env.BUILD_NUMBER}-${MILVUS_CLIENT}-e2e-logs.tar.gz", allowEmptyArchive: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
     }
     // post{
     //     unsuccessful {
